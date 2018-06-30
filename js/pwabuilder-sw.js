5dd0ab049828070cb48bcc64d4d022f35e933a79
Samuel Ayangbola <sam.ayangbola@gmail.com>
2018-06-30 04:19:56 -0700

    Update pwabuilder-sw.js

--- js/pwabuilder-sw.js
+++ js/pwabuilder-sw.js
@@ -9,7 +9,23 @@ var preLoad = function(){
   console.log('[PWA Builder] Install Event processing');
   return caches.open('pwabuilder-offline').then(function(cache) {
     console.log('[PWA Builder] Cached index and offline page during Install');
-    return cache.addAll(['/offline.html', '/index.html']);
+    return cache.addAll([
+  'FX-Converter/',
+	'index.html',
+	'css/index.css',
+	'js/index.js',
+	'img/favicon.png',
+	'img/logo.png',
+	'manifest.json',
+	'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css',
+	'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+	'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js',
+	'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js',
+	'https://free.currencyconverterapi.com/api/v5/currencies',
+	'https://free.currencyconverterapi.com/api/v5/convert',
+	'https://cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js'
+      
+    ]);
   });
 }
 
