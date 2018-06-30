e49e380ea9a45e6f7aaf628fced5e3fdb9fa693f
Samuel Ayangbola <sam.ayangbola@gmail.com>
2018-06-30 06:05:05 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ if (navigator.serviceWorker.controller) {
 } else {
 
 //Register the ServiceWorker
-  navigator.serviceWorker.register('js/pwabuilder-sw.js', {
+  navigator.serviceWorker.register('pwa-sw.js', {
     scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
