502f279063832e2196994a969d889cdbd7bbb284
Samuel Ayangbola <sam.ayangbola@gmail.com>
2018-07-16 15:45:47 -0700

    Update pwa-sw.js

--- pwa-sw.js
+++ pwa-sw.js
@@ -1,15 +1,8 @@
-//This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+//This is the service worker with the Cache-first network
 
-//Install stage sets up the offline page in the cache and opens a new cache
-self.addEventListener('install', function(event) {
-  event.waitUntil(preLoad());
-});
-
-var preLoad = function(){
-  console.log('[FX-Converter] Install Event processing');
-  return caches.open('fx-converter-offline').then(function(cache) {
-    console.log('[FX-Converter] Cached index and offline page during Install');
-    return cache.addAll([
+var CACHE = 'pwabuilder-precache';
+var precacheFiles = [
+      /* Add an array of files to precache for your app */
 	'index.html',
 	'css/index.css',
 	'js/index.js',
@@ -25,63 +18,57 @@ var preLoad = function(){
 	'https://cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js',
 	'https://fonts.googleapis.com/css?family=Righteous',
 	'https://fonts.googleapis.com/icon?family=Material+Icons'
-    
-    ]);
+    ];
+
+//Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function(evt) {
+  console.log('[FX-Converter] The service worker is being installed.');
+  evt.waitUntil(precache().then(function() {
+    console.log('[FX-Converter] Skip waiting on install');
+    return self.skipWaiting();
+  }));
+});
+
+
+//allow sw to control of current page
+self.addEventListener('activate', function(event) {
+  console.log('[FX-Converter] Claiming clients for current page');
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(evt) {
+  console.log('[FX-Converter] The service worker is serving the asset.'+ evt.request.url);
+  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+  evt.waitUntil(update(evt.request));
+});
+
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(precacheFiles);
   });
 }
 
-self.addEventListener('fetch', function(event) {
-  console.log('[FX-Converter] The service worker is serving the asset.');
-  event.respondWith(checkResponse(event.request).catch(function() {
-    return returnFromCache(event.request)}
-  ));
-  event.waitUntil(addToCache(event.request));
-});
-
-var checkResponse = function(request){
-  return new Promise(function(fulfill, reject) {
-    fetch(request).then(function(response){
-      if(response.status !== 404) {
-        fulfill(response)
-      } else {
-        reject()
-      }
-    }, reject)
+function fromCache(request) {
+  //we pull files from the cache first thing so we can show them fast
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
   });
-};
+}
 
-var addToCache = function(request){
-  return caches.open('fx-converter-offline').then(function (cache) {
+function update(request) {
+  //this is where we call the server to get the newest version of the 
+  //file to use the next time we show view
+  return caches.open(CACHE).then(function (cache) {
     return fetch(request).then(function (response) {
-      console.log('[FX-Converter] add page to offline'+response.url)
       return cache.put(request, response);
     });
   });
-};
+}
 
-var returnFromCache = function(request){
-  return caches.open('fx-converter-offline').then(function (cache) {
-    return cache.match(request).then(function (matching) {
-     if(!matching || matching.status == 404) {
-       return cache.match(
-        'index.html',
-	'css/index.css',
-	'js/index.js',
-	'js/manup.js',
-	'img/favicon.png',
-	'img/logo.png',
-	'manifest.json',
-	'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css',
-	'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
-	'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js',
-	'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js',
-	'https://free.currencyconverterapi.com/api/v5/currencies',
-	'https://cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js',   
-      	'https://fonts.googleapis.com/css?family=Righteous',
-       	'https://fonts.googleapis.com/icon?family=Material+Icons')
-     } else {
-       return matching
-     }
-    });
-  });
-};
+function fromServer(request){
+  //this is the fallback if it is not in the cache to go to the server and get it
+  return fetch(request).then(function(response){ return response});
+}
